6aa9ee5c19de392ff8b179b511cda0e188351423
Fahmi Abdul Yakub <fahmi.yakub@atto.id>
2021-08-24 22:47:23 +0700

    posts screen

--- src/screens/Posts/index.js
+++ src/screens/Posts/index.js
@@ -2,10 +2,12 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {useDispatch} from 'react-redux';
 import {getPosts} from '../../configs/redux/action';
+import {colors, wp} from '../../constants';
 
 export const Posts = () => {
   const dispatch = useDispatch();
   const [list_post, setListPost] = useState([]);
+  const [refresh, setRefresh] = useState(false);
   console.log(list_post);
 
   useEffect(() => {
@@ -15,6 +17,14 @@ export const Posts = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const onRefresh = () => {
+    setRefresh(true);
+    dispatch(getPosts()).then(result => {
+      setListPost(result);
+      setRefresh(false);
+    });
+  };
+
   return (
     <View>
       <Text>posts</Text>
@@ -22,4 +32,22 @@ export const Posts = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    paddingHorizontal: wp(3),
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  container: {
+    paddingHorizontal: wp(3),
+  },
+  dot: {
+    backgroundColor: colors.black,
+    width: wp(2.5),
+    height: wp(2.5),
+    borderRadius: wp(2.5) / 2,
+  },
+});
